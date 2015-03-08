d2c002f9c07cc8e543bf2bf92c79de863a3a6cd4
Quelu <quelumatagne@gmail.com>
2015-03-08 02:31:12 +0100

    Use of yo Polymer
    Start of the refactoring for using the polymer generator

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,204 +1,457 @@
-"use strict";
+'use strict';
 
-module.exports = function( grunt ) {
+module.exports = function (grunt) {
+  // show elapsed time at the end
+  require('time-grunt')(grunt);
+  // load all grunt tasks
+  require('load-grunt-tasks')(grunt);
+  grunt.loadNpmTasks('web-component-tester');
 
-    // dynamically loading all grunt files in dev dependencies
-    require( "matchdep" ).filterDev( "grunt-*" ).forEach( grunt.loadNpmTasks );
+  // configurable paths
+  var yeomanConfig = {
+    app: 'app',
+    dist: 'dist'
+  };
 
-    // App Targets, change values during development, accelerating compilation times
-    var oAppTargets = {
-        "mac": false,
-        "win": true,
-        "linux32": false,
-        "linux64": false
-    };
+  // App Targets, change values during development, accelerating compilation times
+  var oAppTargets = {
+    mac: false,
+    win: true,
+    linux32: false,
+    linux64: false
+  };
 
-    // configure tasks
-    grunt.initConfig( {
-        /* bumpup: incrementing version in app's package.json */
-        "bumpup": "src/manifest.json",
-        /* clean: empty folders before working */
-        "clean": {
-            "bin": [ "bin" ],
-            "releases": [ "releases" ]
+  var oBinariesTargets = {
+    releases: 'releases',
+    builds: 'builds'
+  };
+
+  grunt.initConfig({
+    yeoman: yeomanConfig,
+    binaries: oBinariesTargets,
+    bumpup: '<%= yeoman.app %>/manifest.json',
+    watch: {
+      options: {
+        nospawn: true
+      },
+      default: {
+        files: [
+          '<%= yeoman.app %>/*.html',
+          '<%= yeoman.app %>/elements/{,*/}*.html',
+          '{.tmp,<%= yeoman.app %>}/elements/{,*/}*.{css,js}',
+          '{.tmp,<%= yeoman.app %>}/styles/{,*/}*.css',
+          '{.tmp,<%= yeoman.app %>}/scripts/{,*/}*.js',
+          '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+        ]
+      },
+      js: {
+        files: ['<%= yeoman.app %>/scripts/{,*/}*.js'],
+        tasks: ['jshint']
+      },
+      styles: {
+        files: [
+          '<%= yeoman.app %>/styles/{,*/}*.css',
+          '<%= yeoman.app %>/elements/{,*/}*.css'
+        ],
+        tasks: ['copy:styles', 'autoprefixer:server']
+      },
+      sass: {
+        files: [
+          '<%= yeoman.app %>/styles/{,*/}*.{scss,sass}',
+          '<%= yeoman.app %>/elements/{,*/}*.{scss,sass}'
+        ],
+        tasks: ['sass:server', 'autoprefixer:server']
+      }
+    },
+    // Compiles Sass to CSS and generates necessary files if requested
+    sass: {
+      options: {
+        loadPath: 'bower_components'
+      },
+      dist: {
+        options: {
+          style: 'compressed'
         },
-        /* copy: copy static files */
-        "copy": {
-            "manifest": {
-                "src": "src/manifest.json",
-                "dest": "bin/package.json"
-            },
-            "assets": {
-                "expand": true,
-                "src": [ "assets/**/*" ],
-                "cwd": "src/",
-                "dest": "bin/"
-            },
-            "js": {
-                "expand": true,
-                "src": [ "js/**/*" ],
-                "cwd": "src/",
-                "dest": "bin/"
+        files: [{
+          expand: true,
+          cwd: '<%= yeoman.app %>',
+          src: ['styles/{,*/}*.{scss,sass}', 'elements/{,*/}*.{scss,sass}'],
+          dest: '<%= yeoman.dist %>',
+          ext: '.css'
+        }]
+      },
+      server: {
+        files: [{
+          expand: true,
+          cwd: '<%= yeoman.app %>',
+          src: ['styles/{,*/}*.{scss,sass}', 'elements/{,*/}*.{scss,sass}'],
+          dest: '.tmp',
+          ext: '.css'
+        }]
+      }
+    },
+    autoprefixer: {
+      options: {
+        browsers: ['last 2 versions']
+      },
+      server: {
+        files: [{
+          expand: true,
+          cwd: '.tmp',
+          src: '**/*.css',
+          dest: '.tmp'
+        }]
+      },
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '<%= yeoman.dist %>',
+          src: ['**/*.css', '!bower_components/**/*.css'],
+          dest: '<%= yeoman.dist %>'
+        }]
+      }
+    },
+    browserSync: {
+      options: {
+        notify: false,
+        port: 9000,
+        open: true
+      },
+      app: {
+        options: {
+          watchTask: true,
+          injectChanges: false, // can't inject Shadow DOM
+          server: {
+            baseDir: ['.tmp', '<%= yeoman.app %>'],
+            routes: {
+              '/bower_components': 'bower_components'
             }
+          }
         },
-        "bower": {
-            "install": {
-                "options": {
-                    "targetDir": 'bin/components',
-                    "install": true,
-                    "cleanBowerDir": true,
-                    "cleanTargetDir": true
-                }
-            }
+        src: [
+          '.tmp/**/*.{css,html,js}',
+          '<%= yeoman.app %>/**/*.{css,html,js}'
+        ]
+      },
+      dist: {
+        options: {
+          server: {
+            baseDir: 'dist'
+          }
         },
-        /* jade: compile jade files into html */
-        "jade": {
-            "jades": {
-                "options": {
-                    "pretty": true
-                },
-                "files": {
-                    "bin/index.html": "src/jade/index.jade"
-                }
-            }
+        src: [
+          '<%= yeoman.dist %>/**/*.{css,html,js}',
+          '!<%= yeoman.dist %>/bower_components/**/*'
+        ]
+      }
+    },
+    clean: {
+      dist: ['.tmp', '<%= yeoman.dist %>/*', '<%= yeoman.dist %>', '.sass-cache/**/*', '.sass-cache'],
+      releases: '<%= binaries.releases %>',
+      builds: ['<%= binaries.builds %>/**/*', '<%= binaries.builds %>'],
+      server: '.tmp'
+    },
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc',
+        reporter: require('jshint-stylish')
+      },
+      all: [
+        '<%= yeoman.app %>/scripts/{,*/}*.js',
+        '!<%= yeoman.app %>/scripts/vendor/*',
+        'test/spec/{,*/}*.js'
+      ]
+    },
+    useminPrepare: {
+      html: '<%= yeoman.app %>/index.html',
+      options: {
+        dest: '<%= yeoman.dist %>'
+      }
+    },
+    usemin: {
+      html: ['<%= yeoman.dist %>/{,*/}*.html'],
+      css: ['<%= yeoman.dist %>/styles/{,*/}*.css'],
+      options: {
+        dirs: ['<%= yeoman.dist %>']
+      }
+    },
+    replace: {
+      dist: {
+        options: {
+          patterns: [{
+            match: /\/elements\/elements\.html/g,
+            replacement: '/elements/elements.vulcanized.html'
+          }]
         },
-        /* markdown: compile markdown documents, for about text */
-        "markdown": {
-            "credits": {
-                "options": {
-                    "templateContext": {}
-                },
-                "files": [
-                    {
-                        "src": "src/about.md",
-                        "dest": "bin/about.html"
-                    }
-                ]
-            }
-        },
-        /* stylus: styles of the application, including kouto-swiss */
-        "stylus": {
-            "options": {
-                "compress": false,
-                "use": [
-                    require( "kouto-swiss" )
-                ]
-            },
-            "styles": {
-                "files": {
-                    "bin/css/styles.css": "src/stylus/styles.styl"
-                }
-            }
-        },
-        /* install-dependencies: load dependencies of the app before packaginf it */
-        "install-dependencies": {
-            "options": {
-                "cwd": "bin"
-            }
-        },
-        /* node-webkit: package the app for multiple platforms */
-        "nodewebkit": {
-            "options": {
-                "build_dir": "builds",
-                "appName": "analiz",
-                "appVersion": grunt.file.readJSON( "src/manifest.json" ).version,
-                "credits": "bin/about.html",
-                "mac": oAppTargets.mac,
-                "mac_icns": "bin/assets/icons/icon.icns",
-                "win": oAppTargets.win,
-                "linux32": oAppTargets.linux32,
-                "linux64": oAppTargets.linux64,
-                "forceDownload": false
-            },
-            "src": "bin/**/*"
-        },
-        /* compress: packaging the binaries into shippable zip */
-        "compress": {
-            "options": {
-                "pretty": true,
-                "mode": "zip"
-            },
-            "mac": {
-                "options": {
-                    "archive": "releases/mac/analiz.zip"
-                },
-                "files": [
-                    {
-                        "expand": true,
-                        "cwd": "builds/analiz/osx/",
-                        "src": [ "**/*" ]
-                    }
-                ]
-            },
-            "win": {
-                "options": {
-                    "archive": "releases/win/analiz.zip"
-                },
-                "files": [
-                    {
-                        "expand": true,
-                        "cwd": "builds/analiz/win/",
-                        "src": [ "**/*" ]
-                    }
-                ]
-            },
-            "linux32": {
-                "options": {
-                    "archive": "releases/linux32/analiz.zip"
-                },
-                "files": [
-                    {
-                        "expand": true,
-                        "cwd": "builds/analiz/linux32/",
-                        "src": [ "**/*" ]
-                    }
-                ]
-            },
-            "linux64": {
-                "options": {
-                    "archive": "releases/linux64/analiz.zip"
-                },
-                "files":
-                [
-                    {
-                        "expand": true,
-                        "cwd": "builds/analiz/linux64/",
-                        "src": [ "**/*" ]
-                    }
-                ]
-            }
-        },
-        /* watch: watching files and launch some tasks at changes */
-        "watch": {
-            "styles": {
-                "files": "src/stylus/styles.styl",
-                "tasks": [ "stylus" ]
-            },
-            "jade": {
-                "files": "src/jade/**/*",
-                "tasks": [ "jade" ]
-            }
+        files: {
+          '<%= yeoman.dist %>/index.html': ['<%= yeoman.dist %>/index.html']
         }
-    } );
+      }
+    },
+    vulcanize: {
+      default: {
+        options: {
+          strip: true
+        },
+        files: {
+          '<%= yeoman.dist %>/elements/elements.vulcanized.html': [
+            '<%= yeoman.dist %>/elements/elements.html'
+          ]
+        }
+      }
+    },
+    imagemin: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '<%= yeoman.app %>/images',
+          src: '{,*/}*.{png,jpg,jpeg,svg}',
+          dest: '<%= yeoman.dist %>/images'
+        }]
+      }
+    },
+    minifyHtml: {
+      options: {
+        quotes: true,
+        empty: true,
+        spare: true
+      },
+      app: {
+        files: [{
+          expand: true,
+          cwd: '<%= yeoman.dist %>',
+          src: '*.html',
+          dest: '<%= yeoman.dist %>'
+        }]
+      }
+    },
+    copy: {
+      dist: {
+        files: [{
+          expand: true,
+          dot: true,
+          cwd: '<%= yeoman.app %>',
+          dest: '<%= yeoman.dist %>',
+          src: [
+            '*.{ico,txt}',
+            '.htaccess',
+            '*.html',
+            'elements/**',
+            '!elements/**/*.scss',
+            'images/{,*/}*.{webp,gif}'
+          ]
+        }, {
+          expand: true,
+          dot: true,
+          dest: '<%= yeoman.dist %>',
+          src: ['bower_components/**']
+        }]
+      },
+      styles: {
+        files: [{
+          expand: true,
+          cwd: '<%= yeoman.app %>',
+          dest: '.tmp',
+          src: ['{styles,elements}/{,*/}*.css']
+        }]
+      },
+      manifest: {
+        files: [{
+          src: '<%= yeoman.app %>/manifest.json',
+          dest: '<%= yeoman.dist %>/package.json'
+        }]
+      },
+      assets: {
+        files: [{
+          expand: true,
+          src: [ 'assets/**/*' ],
+          cwd: '<%= yeoman.app %>/',
+          dest: '<%= yeoman.dist %>/'
+        }]
+      },
+    },
+    'wct-test': {
+      local: {
+        options: {remote: false}
+      },
+      remote: {
+        options: {remote: true}
+      }
+    },
+    // See this tutorial if you'd like to run PageSpeed
+    // against localhost: http://www.jamescryer.com/2014/06/12/grunt-pagespeed-and-ngrok-locally-testing/
+    pagespeed: {
+      options: {
+        // By default, we use the PageSpeed Insights
+        // free (no API key) tier. You can use a Google
+        // Developer API key if you have one. See
+        // http://goo.gl/RkN0vE for info
+        nokey: true
+      },
+      // Update `url` below to the public URL for your site
+      mobile: {
+        options: {
+          url: "https://developers.google.com/web/fundamentals/",
+          locale: "en_GB",
+          strategy: "mobile",
+          threshold: 80
+        }
+      }
+    },
+    /* markdown: compile markdown documents, for about text */
+    markdown: {
+      credits: {
+        options: {
+          templateContext: {}
+        },
+        files: [
+          {
+            src: '<%= yeoman.app %>/about.md',
+            dest: '<%= yeoman.dist %>/about.html'
+          }
+        ]
+      }
+    },
+    /* install-dependencies: load dependencies of the app before packaginf it */
+    "install-dependencies": {
+      options: {
+        cwd: '<%= yeoman.dist %>'
+      }
+    },
+    /* node-webkit: package the app for multiple platforms */
+    nodewebkit: {
+      options: {
+        build_dir: 'builds',
+        appName: 'analiz',
+        appVersion: grunt.file.readJSON(yeomanConfig.app + '/manifest.json').version,
+        credits: yeomanConfig.dist + '/about.html',
+        mac: oAppTargets.mac,
+        mac_icns: yeomanConfig.dist + '/assets/icons/icon.icns',
+        win: oAppTargets.win,
+        linux32: oAppTargets.linux32,
+        linux64: oAppTargets.linux64,
+        forceDownload: false
+      },
+      src: '<%= yeoman.dist %>/**/*'
+    },
+    /* compress: packaging the binaries into shippable zip */
+    compress: {
+      options: {
+        pretty: true,
+        mode: 'zip'
+      },
+      mac: {
+        options: {
+          archive: '<%= binaries.releases %>/mac/analiz.zip'
+        },
+        files: [
+          {
+            expand: true,
+            cwd: '<%= binaries.builds %>/analiz/osx/',
+            src: [ '**/*' ]
+          }
+        ]
+      },
+      win: {
+        options: {
+          archive: '<%= binaries.releases %>/win/analiz.zip'
+        },
+        files: [
+          {
+            expand: true,
+            cwd: '<%= binaries.builds %>/analiz/win/',
+            src: [ '**/*' ]
+          }
+        ]
+      },
+      linux32: {
+        options: {
+          archive: '<%= binaries.releases %>/linux32/analiz.zip'
+        },
+        files: [
+          {
+            expand: true,
+            cwd: '<%= binaries.builds %>/analiz/linux32/',
+            src: [ '**/*' ]
+          }
+        ]
+      },
+      linux64: {
+        options: {
+          archive: '<%= binaries.releases %>/linux64/analiz.zip'
+        },
+        files:
+        [
+          {
+            expand: true,
+            cwd: '<%= binaries.builds %>/analiz/linux64/',
+            src: [ '**/*' ]
+          }
+        ]
+      }
+    }
+  });
 
-    // declare tasks
-    grunt.registerTask( "default", [ "build" ] );
+  grunt.registerTask('server', function (target) {
+    grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
+    grunt.task.run(['serve:' + target]);
+  });
 
-    grunt.registerTask( "build", [
-        "clean",
-        "bumpup:prerelease",
-        "copy:manifest",
-        "jade",
-        "bower",
-        "copy:js",
-        "copy:assets",
-        "markdown",
-        "stylus",
-        "install-dependencies",
-        "nodewebkit",
-        "compress"
-    ] );
+  grunt.registerTask('serve', function (target) {
+    if (target === 'dist') {
+      return grunt.task.run(['build', 'browserSync:dist']);
+    }
 
-    // TODO : install & configure JSHint tasks
+    grunt.task.run([
+      'clean:server',
+      'sass:server',
+      'copy:styles',
+      'autoprefixer:server',
+      'browserSync:app',
+      'watch'
+    ]);
+  });
 
+  grunt.registerTask('test:local', ['wct-test:local']);
+  grunt.registerTask('test:remote', ['wct-test:remote']);
+
+  grunt.registerTask('build', [
+    'clean:dist',
+    'sass',
+    'copy',
+    'useminPrepare',
+    'imagemin',
+    'concat',
+    'autoprefixer',
+    'uglify',
+    'vulcanize',
+    'usemin',
+    'replace',
+    'minifyHtml'
+  ]);
+
+  grunt.registerTask('build-binaries', [
+    'clean:dist',
+    'bumpup:prerelease',
+    'sass',
+    'copy',
+    'useminPrepare',
+    'imagemin',
+    'concat',
+    'autoprefixer',
+    'uglify',
+    'vulcanize',
+    'usemin',
+    'replace',
+    'minifyHtml',
+    'install-dependencies',
+    'nodewebkit',
+    'compress'
+  ]);
+
+  grunt.registerTask('default', [
+    'jshint',
+    // 'test'
+    'build'
+  ]);
 };

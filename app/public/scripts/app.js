9651bf541ffdbf0d12ccfd2fe8521206394c6e9e
Quelu <quelumatagne@gmail.com>
2015-05-05 01:09:56 +0200

    Layout: sidebar + main pages

--- app/public/scripts/app.js
+++ app/public/scripts/app.js
@@ -5,7 +5,70 @@
   // and give it some initial binding values
   // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
   var app = document.querySelector('#app');
-  app.appName = 'Yo, Polymer App!';
+
+  var colors = [
+    'red',
+    'pink',
+    'purple',
+    'deep-purple',
+    'indigo',
+    'blue',
+    'light-blue',
+    'cyan',
+    'teal',
+    'green',
+    'light-green',
+    'lime',
+    'yellow',
+    'amber',
+    'orange',
+    'deep-orange',
+    'brown',
+    'grey',
+    'blue-grey'
+  ];
+
+  app.appName = 'Analyz';
+  app.selected_page = 0;
+
+  var i = 0;
+  app.filters = [
+    {
+      'id': i++,
+      'name': "Validation W3C",
+      'color': colors[Math.floor(Math.random()*colors.length)]
+    },
+    {
+      'id': i++,
+      'name': "Analyse syntaxique",
+      'color': colors[Math.floor(Math.random()*colors.length)]
+    },
+    {
+      'id': i++,
+      'name': "Plan du document",
+      'color': colors[Math.floor(Math.random()*colors.length)]
+    },
+    {
+      'id': i++,
+      'name': "Rapport sur les feuilles de styles",
+      'color': colors[Math.floor(Math.random()*colors.length)]
+    },
+    {
+      'id': i++,
+      'name': "Microdatas et microformats",
+      'color': colors[Math.floor(Math.random()*colors.length)]
+    },
+    {
+      'id': i++,
+      'name': "Attributs ARIA",
+      'color': colors[Math.floor(Math.random()*colors.length)]
+    },
+    {
+      'id': i++,
+      'name': "Contraste",
+      'color': colors[Math.floor(Math.random()*colors.length)]
+    },
+  ];
 
   // Listen for template bound event to know when bindings
   // have resolved and content has been stamped to the page

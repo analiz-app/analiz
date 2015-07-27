28f1cc563c78eb47d7393204318e717d6bcf0731
Quelu <lucmatagne@hotmail.com>
2015-07-28 00:01:23 +0200

    About modal

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,18 +1,18 @@
-(function (document) {
+(function ( document ) {
   'use strict';
 
-  var app = document.querySelector('#app');
+  var app = document.querySelector( '#app' );
 
   app.appName = "Analiz";
-  
-  app.remote = require('remote');
-  app.npm = require('npm');
-  app.async = require('async');
-  app.categories = require('./categories.js');
-  app.getFilters = require('./moduleLister.js');
-  app.directory = require('directory-tree');
-  app.directory.fs = require('fs');
-  app.dateFormat = require('dateformat');
+
+  app.remote = require( 'remote' );
+  app.npm = require( 'npm' );
+  app.async = require( 'async' );
+  app.categories = require( './categories.js' );
+  app.getFilters = require( './moduleLister.js' );
+  app.directory = require( 'directory-tree' );
+  app.directory.fs = require( 'fs' );
+  app.dateFormat = require( 'dateformat' );
 
   app.npmPrefix = 'resources/app';
   app.filters = [];
@@ -23,8 +23,14 @@
 
   var i = 0;
 
-  window.addEventListener('WebComponentsReady', function() {
-    document.querySelector('body').removeAttribute('unresolved');
+  window.addEventListener( 'WebComponentsReady', function() {
+    document.querySelector( 'body' ).removeAttribute( 'unresolved' );
   });
+
+  // About dialog
+  app.aboutOpen = function ( e ) {
+    document.querySelector( '.settings-dropdown' ).close();
+    document.querySelector( 'about-dialog paper-dialog' ).open();
+  };
 
 })(document);

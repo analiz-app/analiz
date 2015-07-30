d77fd8b58833d896b1aef9346cf022f8c53f7106
Quelu <lucmatagne@hotmail.com>
2015-07-30 13:04:22 +0200

    Add the gulp task for bump releases version

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,18 +1,19 @@
+var bump = require('gulp-bump');
 var del = require('del');
 var electron = require('gulp-electron');
+var filter = require('gulp-filter');
+var git = require('gulp-git');
 var gulp = require('gulp');
-var install = require("gulp-install");
+var install = require('gulp-install');
 var merge = require('merge-stream');
-var run = require('gulp-run');
+var tag_version = require('gulp-tag-version');
 
 var packageJson = require('./app/package.json');
 
 // CONFIG
 var electronVersion = 'v0.30.0';
-// Set your platform as for dev
-var sCurrentPlatform = 'linux-x64';
-// var aPlatforms = [sCurrentPlatform];
-var aPlatforms = [sCurrentPlatform, 'win32-x64', 'darwin-x64'];
+// Set the platforms for builds
+var aPlatforms = ['linux-x64', 'win32-x64', 'darwin-x64'];
 
 // Misc
 var sBuildFolder = 'builds/' + electronVersion + '/' + sCurrentPlatform;
@@ -79,3 +80,36 @@ gulp.task('update', [], function () {
   return gulp.src(['app/**/*'])
     .pipe(gulp.dest(sAppFolder));
 });
+
+/**
+ * Bumping version number and tagging the repository with it.
+ * Please read http://semver.org/
+ *
+ * You can use the commands
+ *
+ *     gulp patch     # makes v0.1.0 → v0.1.1
+ *     gulp feature   # makes v0.1.1 → v0.2.0
+ *     gulp release   # makes v0.2.1 → v1.0.0
+ *
+ * To bump the version numbers accordingly after you did a patch,
+ * introduced a feature or made a backwards-incompatible release.
+ */
+var inc = function (importance) {
+  // get all the files to bump version in
+  return gulp.src(['./package.json', './bower.json', './app/package.json'])
+    // bump the version number in those files
+    .pipe(bump({type: importance}))
+    // save it back to filesystem
+    .pipe(gulp.dest('./'))
+    // commit the changed version number
+    .pipe(git.commit('bumps package version'))
+
+    // read only one file to get the version number
+    .pipe(filter('package.json'))
+    // **tag it in the repository**
+    .pipe(tag_version());
+};
+
+gulp.task('patch', function() { return inc('patch'); });
+gulp.task('feature', function() { return inc('minor'); });
+gulp.task('release', function() { return inc('major'); });
